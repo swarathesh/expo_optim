aa0f9025652df6f112158a997e15707b790a530d
swarathesh <swarathesh60@gmail.com>
2017-08-08 18:49:34 +0530

    closest gift

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
+//    Closest Gift
+//    You are on your way to find the gifts. All the gifts lie in your path in a straight line at prime numbers and your house is at 0.Given
+//    your current position find the closest gift to your position, and calculate the distance between your current position and gift and tell the distance.
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        int result = solve(a, b);
-        result = result / c;
-        result = result %d;
-        System.out.print(result);
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum=0;
+        if(n>1){
+            int count=0;
+            for(int i=2;i<=(int)Math.sqrt(n);i++){
+                if(n%i==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                sum=0;
+            }
+            else{
+                int f=0;
+                for(int i=n+1;i>0;i++){
+                    count=0;
+                    for(int j=2;j<=(int)Math.sqrt(i);j++){
+                        if(i%j==0){
+                            count++;
+                            break;
+                        }
+                    }
+                    if(count==0){
+                        f=i;
+                        break;
+                    }
+                }
+                sum=f-n;
+            }
+        }
+        else{
+            sum=2-n;
+        }
+        System.out.println(sum);
+
 
     }
 
-    private static int solve(int x, int n) {
 
-               if(n==0){
-
-                        return 1;}
-
-                       else if(n%2 == 0)   {
-
-                        return  solve(x*x,n/2);}
-             else    {
-
-                        return x* solve(x*x,(n-1)/2);}
-                        //push
-    }
 
 }
 
