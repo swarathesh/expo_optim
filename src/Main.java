e8ed8436e3efbf36342c7200f24bed09b62c1aa3
swarathesh <swarathesh60@gmail.com>
2017-08-08 18:21:23 +0530

    formula

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int Number1 = scanner.nextInt();
-        int Number2 = scanner.nextInt();
-        int result = Gcd(Number1,Number2);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int result = solve(a, b);
+        result = result / c;
+        result = result %d;
         System.out.print(result);
 
     }
 
-    private static int Gcd(int number1, int number2) {
-        int min = Math.min(number1,number2);
-        int gcd = 0 ;
-        for (int i = min; i>0 ; i--){
-            if(number1%i==0 && number2%i==0){
-                gcd = i;
-                return gcd;
+    private static int solve(int x, int n) {
 
+               if(n==0){
 
-            }
-        }
-        return gcd;
+                        return 1;}
 
+                       else if(n%2 == 0)   {
+
+                        return  solve(x*x,n/2);}
+             else    {
+
+                        return x* solve(x*x,(n-1)/2);}
     }
 
-
 }
+
+
+
