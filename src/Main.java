97f60d8a013255d4bf65367975bfa72fc2da4a77
swarathesh <swarathesh60@gmail.com>
2017-08-08 19:39:04 +0530

    gas station

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-//    Closest Gift
-//    You are on your way to find the gifts. All the gifts lie in your path in a straight line at prime numbers and your house is at 0.Given
-//    your current position find the closest gift to your position, and calculate the distance between your current position and gift and tell the distance.
+//    Gas Stations
+//    Xenny's is competing in a race and his car has
+//    X
+//    X litres of fuel. There are
+//    N
+//    N milestones in the competition. It takes no fuel at all to travel between gas stations, but at the
+//            i
+//    t
+//            h
+//    ith gas station,
+//    P
+//            i
+//    Pi amount of petrol is drained.
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int sum=0;
-        if(n>1){
-            int count=0;
-            for(int i=2;i<=(int)Math.sqrt(n);i++){
-                if(n%i==0){
-                    count++;
-                    break;
-                }
-            }
-            if(count==0){
-                sum=0;
-            }
-            else{
-                int f=0;
-                for(int i=n+1;i>0;i++){
-                    count=0;
-                    for(int j=2;j<=(int)Math.sqrt(i);j++){
-                        if(i%j==0){
-                            count++;
-                            break;
-                        }
-                    }
-                    if(count==0){
-                        f=i;
-                        break;
-                    }
-                }
-                sum=f-n;
-            }
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int[] i = new int[100];
+        for (int j = 0 ; j<a ;j++){
+            int c = scanner.nextInt();
+            i[j] = c;
         }
-        else{
-            sum=2-n;
+        int j=0 ,count=0;
+        while(b>0){
+            b = b-i[j++];
+            count++;
+
         }
-        System.out.println(sum);
+        System.out.print(count);
 
 
     }
